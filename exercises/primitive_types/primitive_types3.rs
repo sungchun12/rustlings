2465f67bf8d5da61dbb981bb5ba86339444de93b
Sung Won Chung <sung@datafold.com>
2023-12-30 10:50:48 -0800

    finished more

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,10 +5,10 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    let a = ???
+    let a = [10; 100]; // Initialize array with 100 elements, all set to 10
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
