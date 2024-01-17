735ab5dc7e3133ad6cdc0fca5e3262a5497bca8b
Sung Won Chung <sung@datafold.com>
2024-01-16 19:24:01 -0800

    more exercises!

--- exercises/tests/tests4.rs
+++ exercises/tests/tests4.rs
@@ -30,17 +30,23 @@ mod tests {
     fn correct_width_and_height() {
         // This test should check if the rectangle is the size that we pass into its constructor
         let rect = Rectangle::new(10, 20);
-        assert_eq!(???, 10); // check width
-        assert_eq!(???, 20); // check height
+        // I notice calling attributes of struct is similar to python when calling class attributes
+        assert_eq!(rect.width, 10); // check width
+        assert_eq!(rect.height, 20); // check height
     }
 
+    // https://doc.rust-lang.org/stable/book/ch11-01-writing-tests.html#checking-for-panics-with-should_panic
+    // https://doc.rust-lang.org/stable/rust-by-example/testing/unit_testing.html
+    // I really like this syntax for checking panics
     #[test]
+    #[should_panic]
     fn negative_width() {
         // This test should check if program panics when we try to create rectangle with negative width
         let _rect = Rectangle::new(-10, 10);
     }
 
     #[test]
+    #[should_panic]
     fn negative_height() {
         // This test should check if program panics when we try to create rectangle with negative height
         let _rect = Rectangle::new(10, -10);
